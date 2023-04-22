ebcd36f342f8eaae5be7dc9fe59d243aa9d02363
Anton <antony.adamson@gmail.com>
2023-04-22 15:44:45 +0600

    Implement UITableViewDataSource

--- DC-Lab4-Meal Tracker/Controller/FoodTableViewController.swift
+++ DC-Lab4-Meal Tracker/Controller/FoodTableViewController.swift
@@ -9,6 +9,20 @@ import UIKit
 
 class FoodTableViewController: UITableViewController {
 
+    var meals: [Meal] {
+        let breakfast = Meal(name: "Breakfast", food: [Food(name: "Coffee", description: "---"),
+                                                      Food(name: "Eggs", description: "---"),
+                                                      Food(name: "Toast", description: "---")])
+        let lunch = Meal(name: "Lunch", food: [Food(name: "Juice", description: "---"),
+                                              Food(name: "Soup", description: "---"),
+                                              Food(name: "Cupcake", description: "---")])
+        let dinner = Meal(name: "Dinner", food: [Food(name: "Beer", description: "---"),
+                                                Food(name: "Steak", description: "---"),
+                                                Food(name: "Potato chips", description: "---")])
+        
+        return [breakfast, lunch, dinner]
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,68 +36,36 @@ class FoodTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return meals.count
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        //добираемся до подмассива в массиве meals через команду section
+        let meal = meals[section]
+        // добираемся до свойства food через подмассив meal обращением meal.food
+        let food = meal.food
+        //возвращаем количество упоминаний свойства food
+        return food.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Food", for: indexPath)
+        //снова добираемся до подмассива через indexPath.section
+        let meal = meals[indexPath.section]
+        //снова в food записываем через обращение meal.food значение по indexPath.row
+        let food = meal.food[indexPath.row]
+        var content = cell.defaultContentConfiguration()
+        content.text = food.name
+        content.secondaryText = food.description
+        cell.contentConfiguration = content
+        
         return cell
     }
-    */
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let meal = meals[section]
+        return meal.name
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
