ad95ea96b5108ff6fdf8a33d0254581025a0af7a
Anton <antony.adamson@gmail.com>
2023-04-22 14:38:21 +0600

    Create Model Objects

---
+++ DC-Lab4-Meal Tracker/Model/Food.swift
@@ -0,0 +1,13 @@
+//
+//  Food.swift
+//  DC-Lab4-Meal Tracker
+//
+//  Created by Антон Адамсон on 22.04.2023.
+//
+
+import Foundation
+
+struct Food {
+    var name: String
+    var description: String
+}
---
+++ DC-Lab4-Meal Tracker/Model/Meal.swift
@@ -0,0 +1,13 @@
+//
+//  Meal.swift
+//  DC-Lab4-Meal Tracker
+//
+//  Created by Антон Адамсон on 22.04.2023.
+//
+
+import Foundation
+
+struct Meal {
+    var name: String
+    var food: [Food]
+}
